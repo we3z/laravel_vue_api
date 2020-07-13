d1d1ecaa499a3ebc3fdcb249d0ed871d0c727c6f
zlq <bluer1236@163.com>
2020-07-13 10:17:04 +0800

    接口修改

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -21,7 +21,6 @@ class AuthController extends BaseController
      */
     public function login(Request $request, ManagerService $managerService)
     {
-<<<<<<< HEAD
         // 验证数据
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email',
@@ -32,37 +31,6 @@ class AuthController extends BaseController
             'email.email' => BaseConst::$LOGIN_EMAIL_FORMAT_MSG,
             'password.required' => BaseConst::$LOGIN_NO_PASSWORD_MSG,
             'password.string' => BaseConst::$LOGIN_PASSWORD_TYPE_MSG,
-=======
-        $request->validate([
-            'name' => 'required|string',
-            'password' => 'required|string',
-            'remember_me' => 'boolean'
-        ]);
-
-        $credentials = request(['name', 'password']);
-
-        if(!Auth::attempt($credentials))
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
-
-        $user = $request->user();
-
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-
-        if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
-
-        $token->save();
-
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
->>>>>>> 82bb3c0a3d13fef650188e086a837270ec4bf189
         ]);
         if ($validator->fails()) {
             return $this->jsonReturn(BaseConst::$HTTP_ERROR_BAD_REQUEST_CODE, $validator->errors()->first(), []);
